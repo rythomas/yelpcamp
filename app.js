641aa18674dbf3a6f1019e1223bd9c875e7b78c4
Ryan Thomas <rythomas@syr.edu>
2018-12-22 18:15:21 +0000

    Add mongolab

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ var commentRoutes    = require("./routes/comments"),
     campgroundRoutes = require("./routes/campgrounds"),
     indexRoutes      = require("./routes/index");
     
-mongoose.connect("mongodb://localhost:27017/yelp_camp", {useNewUrlParser: true});
+mongoose.connect("mongodb://user:password1@ds141924.mlab.com:41924/yelpcamp", {useNewUrlParser: true});
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
