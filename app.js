58b58d4d3424d0a4956ec72581d3e5f01ad3a299
Ryan Thomas <rythomas@syr.edu>
2018-12-26 18:47:09 +0000

    env variable 3

--- app.js
+++ app.js
@@ -16,10 +16,10 @@ var commentRoutes    = require("./routes/comments"),
     campgroundRoutes = require("./routes/campgrounds"),
     indexRoutes      = require("./routes/index");
 
-var url = (process.env.DATABASEURL)  || ("mongodb://localhost:27107/yelp_camp", {useNewUrlParser: true});
-//mongoose.connect(url);
-
-mongoose.connect("mongodb://user:password1@ds141924.mlab.com:41924/yelpcamp", {useNewUrlParser: true});
+var url = process.env.DATABASEURL  || ("mongodb://localhost:27107/yelp_camp", {useNewUrlParser: true});
+mongoose.connect(process.env.DATABASEURL, {useNewUrlParser: true});
+    
+//mongoose.connect("mongodb://user:password1@ds141924.mlab.com:41924/yelpcamp", {useNewUrlParser: true});
 console.log(process.env.DATABASEURL);
 
 app.use(bodyParser.urlencoded({extended: true}));
