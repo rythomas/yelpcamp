f2ddd765589b2f357c5ce4488833110cdb7acf3a
Ryan Thomas <rythomas@syr.edu>
2019-01-07 17:34:56 +0000

    Add User Profiles and Password Reset

--- models/user.js
+++ models/user.js
@@ -3,7 +3,13 @@ var passportLocalMongoose = require("passport-local-mongoose");
 
 var UserSchema = new mongoose.Schema({
     username: String,
-    password: String
+    password: String,
+    avatar:   String,
+    firstName: String,
+    lastName:  String,
+    email: {type: String, unique: true, required: true},
+    resetPasswordToken: String,
+    resetPasswordExpires: Date
 });
 
 UserSchema.plugin(passportLocalMongoose);
